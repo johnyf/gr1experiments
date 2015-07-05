b07535322154dcea0c8c08f3b8b4937f1dc94cd9
Ioannis Filippidis <jfilippidis@gmail.com>
2015-07-05 01:24:55 -0700

    split buddy into pyx and pyd to avoid namespace conflict of `BDD`

--- src/buddy.pyx
+++ src/buddy.pyx
@@ -16,103 +16,7 @@ import sys
 from libcpp cimport bool
 from libc.stdio cimport FILE, fdopen, fopen
 from cpython.mem cimport PyMem_Malloc, PyMem_Free
-
-
-cdef extern from 'bdd.h':
-    # BDD
-    ctypedef int BDD
-    # renaming pair
-    cdef struct s_bddPair:
-        pass
-    ctypedef s_bddPair bddPair
-    cdef int bdd_init(int BDDsize, int cachesize)
-    cdef int bdd_isrunning()
-    cdef void bdd_done()
-    cdef int bdd_setacheratio(int r)
-    # variable creation
-    cdef BDD bdd_ithvar(int var)
-    cdef BDD bdd_nithvar(int var)
-    cdef int bdd_var2level(int var)
-    cdef int bdd_level2var(int level)
-    cdef int bdd_setvarnum(int num)
-    cdef int bdd_extvarnum(int num)
-    cdef int bdd_varnum()
-    # variable manipulation
-    cdef int bdd_var(BDD r)
-    cdef BDD bdd_makeset(int *varset, int varnum)
-    cdef int bdd_scanset(BDD a, int **varset, int *varnum)
-    cdef BDD bdd_ibuildcube(int value, int width, int *var)
-    cdef BDD bdd_buildcube(int value, int width, BDD *var)
-    # BDD elements
-    cdef BDD bdd_true()
-    cdef BDD bdd_false()
-    cdef BDD bdd_low(BDD r)
-    cdef BDD bdd_high(BDD r)
-    cdef BDD bdd_support(BDD r)
-    cdef BDD bdd_satone(BDD r)  # cube
-    cdef BDD bdd_fullsatone(BDD r)  # minterm
-    cdef double bdd_satcount(BDD r)
-    cdef int bdd_nodecount(BDD r)
-    # refs
-    cdef BDD bdd_addref(BDD r)
-    cdef BDD bdd_delref(BDD r)
-    cdef void bdd_gbc()
-    # basic Boolean operators
-    cdef bdd_ite(BDD u, BDD v, BDD w)
-    cdef bdd_apply(BDD u, BDD w, int op)
-    cdef BDD bdd_not(BDD u)
-    cdef BDD bdd_and(BDD u, BDD v)
-    cdef BDD bdd_or(BDD u, BDD v)
-    cdef BDD bdd_xor(BDD u, BDD v)
-    cdef BDD bdd_imp(BDD u, BDD v)
-    cdef BDD bdd_biimp(BDD u, BDD v)
-    # composition operators
-    cdef BDD bdd_restrict(BDD r, BDD var)
-    cdef BDD bdd_constrain(BDD f, BDD c)
-    cdef BDD bdd_compose(BDD f, BDD g, BDD v)
-    cdef bdd_simplify(BDD f, BDD d)
-    # quantification
-    cdef BDD bdd_exist(BDD r, BDD var)
-    cdef BDD bdd_forall(BDD r, BDD var)
-    cdef BDD bdd_appex(BDD u, BDD v, int op, BDD var)
-    cdef BDD bdd_appall(BDD u, BDD v, int op, BDD var)
-    # renaming
-    cdef BDD bdd_replace(BDD r, bddPair *pair)
-    cdef bddPair * bdd_newpair()
-    cdef void bdd_freepair(bddPair *p)
-    cdef int bdd_setpair(bddPair *pair, int oldvar, int newvar)
-    cdef int bdd_setpairs(bddPair *pair, int *oldvar,
-                          int *newvar, int size)
-    cdef void bdd_resetpair(bddPair *pair)
-    cdef void bdd_freepair(bddPair *p)
-    # manager config
-    cdef int bdd_setmaxBDDnum(int size)
-    cdef int bdd_setmaxincrease(int size)
-    cdef int bdd_setminfreeBDDs(int mf)
-    cdef int bdd_getnodenum()
-    cdef int bdd_getallocnum()  # both dead and active
-    # reordering
-    cdef int bdd_addvarblock(BDD b, int fixed)
-    cdef int bdd_intaddvarblock(int first, int last, int fixed)
-    cdef void bdd_varblockall()
-    cdef void bdd_reorder(int method)
-    cdef int bdd_autoreorder(int method)
-    cdef int bdd_autoreorder_times(int method, int num)
-    cdef void bdd_enable_reorder()
-    cdef void bdd_disable_reorder()
-    cdef int bdd_reorder_gain()
-    cdef void bdd_setcacheratio(int r)
-    # I/O
-    cdef int bdd_save(FILE *ofile, BDD r)
-    cdef int bdd_load(FILE *ifile, BDD r)
-    # info
-    cdef int bdd_reorder_verbose(int value)
-    cdef void bdd_printorder()
-    cdef void bdd_fprintorder(FILE *ofile)
-    # cdef void bdd_stats(bddStat *stat)
-    # cdef void bdd_cachestats(bddCacheStat *s)
-    cdef void bdd_fprintstat(FILE *f)
-    cdef void bdd_printstat()
+cimport buddy
 APPLY_MAP = {
     'and': 0, 'xor': 1, 'or': 2, 'nand': 3, 'nor': 4,
     'imp': 5, 'biimp': 6, 'diff': 7, 'less': 8, 'invimp': 9}
@@ -131,7 +35,7 @@ BDD_REORDER_FIXED = 1
 logger = logging.getLogger(__name__)
 
 
-cdef class BuddyBDD(object):
+cdef class BDD(object):
     """Wrapper of BuDDy.
 
     Interface similar to `dd.bdd.BDD`.
@@ -148,19 +52,19 @@ cdef class BuddyBDD(object):
         n_nodes = 10**2
         cache = 10**4
         n_vars = 150
-        bdd_init(n_nodes, cache)
-        bdd_setvarnum(n_vars)
-        bdd_setcacheratio(64)
-        bdd_autoreorder(BDD_REORDER_SIFT)
-        bdd_reorder_verbose(1)
+        buddy.bdd_init(n_nodes, cache)
+        buddy.bdd_setvarnum(n_vars)
+        buddy.bdd_setcacheratio(64)
+        buddy.bdd_autoreorder(BDD_REORDER_SIFT)
+        buddy.bdd_reorder_verbose(1)
 
     def __dealloc__(self):
-        bdd_done()
+        buddy.bdd_done()
 
     def __str__(self):
-        n = bdd_getnodenum()
-        n_alloc = bdd_getallocnum()
-        n_vars = bdd_varnum()
+        n = buddy.bdd_getnodenum()
+        n_alloc = buddy.bdd_getallocnum()
+        n_vars = buddy.bdd_varnum()
         s = (
             'Binary decision diagram (BuDDy wrapper) with:\n'
             '\t {n} live nodes now\n'
@@ -170,13 +74,13 @@ cdef class BuddyBDD(object):
         return s
 
     def __len__(self):
-        return bdd_getnodenum()
+        return buddy.bdd_getnodenum()
 
     cdef incref(self, int u):
-        bdd_addref(u)
+        buddy.bdd_addref(u)
 
     cdef decref(self, int u):
-        bdd_delref(u)
+        buddy.bdd_delref(u)
 
     property False:
 
@@ -190,9 +94,9 @@ cdef class BuddyBDD(object):
 
     cdef _bool(self, b):
         if b:
-            r = bdd_true()
+            r = buddy.bdd_true()
         else:
-            r = bdd_false()
+            r = buddy.bdd_false()
         return Function(r)
 
     cpdef int add_var(self, str var):
@@ -203,7 +107,7 @@ cdef class BuddyBDD(object):
         j = len(self.var_to_index)
         self.var_to_index[var] = j
         # new block for reordering
-        bdd_intaddvarblock(j, j, 0)
+        buddy.bdd_intaddvarblock(j, j, 0)
         return j
 
     cpdef Function var(self, str var):
@@ -211,19 +115,19 @@ cdef class BuddyBDD(object):
         assert var in self.var_to_index, (
             var, self.var_to_index)
         j = self.var_to_index[var]
-        r = bdd_ithvar(j)
+        r = buddy.bdd_ithvar(j)
         assert r != self.False.node, 'failed'
-        bdd_intaddvarblock(j, j, 0)
+        buddy.bdd_intaddvarblock(j, j, 0)
         return Function(r)
 
     cpdef int level(self, str var):
         """Return level of variable `var`."""
         j = self.add_var(var)
-        level = bdd_var2level(j)
+        level = buddy.bdd_var2level(j)
         return level
 
     cpdef int at_level(self, int level):
-        level = bdd_level2var(level)
+        level = buddy.bdd_level2var(level)
         index_to_var = {
             v: k for k, v in self.var_to_index.iteritems()}
         j = index_to_var[level]
@@ -234,24 +138,24 @@ cdef class BuddyBDD(object):
         # unary
         if op in ('!', 'not'):
             assert v is None
-            r = bdd_not(u.node)
+            r = buddy.bdd_not(u.node)
         else:
             assert v is not None
         # binary
         if op in ('&', 'and'):
-            r = bdd_and(u.node, v.node)
+            r = buddy.bdd_and(u.node, v.node)
         elif op in ('|', 'or'):
-            r = bdd_or(u.node, v.node)
+            r = buddy.bdd_or(u.node, v.node)
         elif op in ('^', 'xor'):
-            r = bdd_xor(u.node, v.node)
+            r = buddy.bdd_xor(u.node, v.node)
         return Function(r)
 
     cpdef quantify(self, u, qvars, forall=False):
         cube = self.cube(qvars)
         if forall:
-            r = bdd_forall(u, cube)
+            r = buddy.bdd_forall(u, cube)
         else:
-            r = bdd_exist(u, cube)
+            r = buddy.bdd_exist(u, cube)
         return Function(r)
 
     cpdef cube(self, dvars):
@@ -263,7 +167,7 @@ cdef class BuddyBDD(object):
             j = self.add_var(var)
             x[i] = j
         try:
-            r = bdd_makeset(x, n)
+            r = buddy.bdd_makeset(x, n)
         finally:
             PyMem_Free(x)
         return Function(r)
@@ -277,7 +181,7 @@ cpdef and_abstract(u, v, qvars, bdd):
     """Return `? qvars. u & v`."""
     cube = bdd.cube(qvars)
     op = APPLY_MAP['and']
-    r = bdd_appex(u.node, v.node, op, cube.node)
+    r = buddy.bdd_appex(u.node, v.node, op, cube.node)
     return Function(r)
 
 
@@ -285,7 +189,7 @@ cpdef or_abstract(u, v, qvars, bdd):
     """Return `! qvars. u | v`."""
     cube = bdd.cube(qvars)
     op = APPLY_MAP['or']
-    r = bdd_appall(u.node, v.node, op, cube.node)
+    r = buddy.bdd_appall(u.node, v.node, op, cube.node)
     return Function(r)
 
 
@@ -302,10 +206,10 @@ def rename(u, bdd, dvars):
         newvars[i] = jb
     cdef bddPair *pair = bdd_newpair()
     try:
-        bdd_setpairs(pair, oldvars, newvars, n)
-        r = bdd_replace(u.node, pair)
+        buddy.bdd_setpairs(pair, oldvars, newvars, n)
+        r = buddy.bdd_replace(u.node, pair)
     finally:
-        bdd_freepair(pair)
+        buddy.bdd_freepair(pair)
         PyMem_Free(oldvars)
         PyMem_Free(newvars)
     return Function(r)
@@ -332,10 +236,10 @@ cdef class Function(object):
 
     def __cinit__(self, node):
         self.node = node
-        bdd_addref(node)
+        buddy.bdd_addref(node)
 
     def __dealloc__(self):
-        bdd_delref(self.node)
+        buddy.bdd_delref(self.node)
 
     def __str__(self):
         n = len(self)
@@ -343,7 +247,7 @@ cdef class Function(object):
             u=self.node, n=n)
 
     def __len__(self):
-        return bdd_nodecount(self.node)
+        return buddy.bdd_nodecount(self.node)
 
     def __richcmp__(self, other, op):
         if other is None:
@@ -358,13 +262,13 @@ cdef class Function(object):
             raise Exception('Only `==` and `!=` defined.')
 
     def __invert__(self):
-        r = bdd_not(self.node)
+        r = buddy.bdd_not(self.node)
         return Function(r)
 
     def __and__(self, other):
-        r = bdd_and(self.node, other.node)
+        r = buddy.bdd_and(self.node, other.node)
         return Function(r)
 
     def __or__(self, other):
-        r = bdd_or(self.node, other.node)
+        r = buddy.bdd_or(self.node, other.node)
         return Function(r)
