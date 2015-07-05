ab526f08744d978d5757aa54d5e0f075079fd47f
Ioannis Filippidis <jfilippidis@gmail.com>
2015-07-04 19:05:32 -0700

    update setup to compile BuDDy bindings

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ import os
 from distutils.core import setup
 from Cython.Build import cythonize
 
-os.environ['CC'] = "gcc"
+os.environ['CC'] = 'gcc'
 
 setup(
-    ext_modules=cythonize("cudd.pyx"))
+    ext_modules=cythonize('buddy.pyx', gdb_debug=True))
