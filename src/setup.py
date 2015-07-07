6b3e7425961a870d6d912ac569282232638d7d0f
Ioannis Filippidis <jfilippidis@gmail.com>
2015-07-07 04:23:01 -0700

    mv cudd and buddy modules to `dd` package

--- src/setup.py
+++
@@ -1,8 +0,0 @@
-import os
-from distutils.core import setup
-from Cython.Build import cythonize
-
-os.environ['CC'] = 'gcc'
-
-setup(
-    ext_modules=cythonize('cudd.pyx', gdb_debug=True))
--- src/test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("void *: %lu\n int: %lu\n", sizeof(void *), sizeof(int) );
-    return 0;
-}
