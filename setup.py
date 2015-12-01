266a143e4c54bdca710bf63c919266c4c63171c2
Ioannis Filippidis <jfilippidis@gmail.com>
2015-12-01 13:22:37 -0800

    REL: retrieve commit hash from git and recognize version tags

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+# inline
+# import git
 
 
 name = 'tugs'
@@ -10,11 +12,11 @@ VERSION_FILE = '{name}/_version.py'.format(name=name)
 MAJOR = 0
 MINOR = 1
 MICRO = 1
-version = '{major}.{minor}.{micro}'.format(
+VERSION = '{major}.{minor}.{micro}'.format(
     major=MAJOR, minor=MINOR, micro=MICRO)
-s = (
+VERSION_TEXT = (
     '# This file was generated from setup.py\n'
-    "version = '{version}'\n").format(version=version)
+    "version = '{version}'\n")
 install_requires = [
     'dd >= 0.1.3',
     'omega >= 0.0.3']
@@ -22,7 +24,33 @@ tests_require = [
     'nose >= 1.3.4']
 
 
+def git_version(version):
+    try:
+        import git
+        repo = git.Repo('.git')
+        repo.git.status()
+    except ImportError, git.GitCommandNotFound:
+        print('gitpython or git not found: Assume release.')
+        return ''
+    sha = repo.head.commit.hexsha
+    if repo.is_dirty():
+        return '.dev0+{sha}-dirty'.format(sha=sha)
+    # commit is clean
+    # is it release of `version` ?
+    try:
+        tag = repo.git.describe(
+            match='v[0-9]*', exact_match=True,
+            tags=True, dirty=True)
+        assert tag[1:] == version, (tag, version)
+        return ''
+    except git.GitCommandError:
+        return '.dev0+{sha}'.format(sha=sha)
+
+
 def run_setup():
+    # version
+    version = VERSION + git_version(VERSION)
+    s = VERSION_TEXT.format(version=version)
     with open(VERSION_FILE, 'w') as f:
         f.write(s)
     setup(
