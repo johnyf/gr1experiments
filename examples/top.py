16ef79ce48af273fed3c3824e0853b895707dc86
Ioannis Filippidis <jfilippidis@gmail.com>
2016-02-09 14:16:09 -0800

    ENH: show total virtual memory in top

--- examples/top.py
+++ examples/top.py
@@ -42,9 +42,12 @@ def main():
     total_rss_str = humanize.naturalsize(total_rss)
     total_vms = sum(vms_all)
     total_vms_str = humanize.naturalsize(total_vms)
+    total_memory = psutil.virtual_memory().total
+    total_memory_str = humanize.naturalsize(total_memory)
     print('\n' + 60 * '-')
     print('total RSS: {m}'.format(m=total_rss_str))
     print('total VMS: {m}'.format(m=total_vms_str))
+    print('total memory: {m}'.format(m=total_memory_str))
 
 
 if __name__ == '__main__':
