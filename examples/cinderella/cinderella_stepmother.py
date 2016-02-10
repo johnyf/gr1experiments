779b06090f476cc5fadf0b5652a8cfeab189311a
Ioannis Filippidis <jfilippidis@gmail.com>
2016-02-10 01:20:33 -0800

    ENH: convert cinderella to open Promela generator
    to use a bitblaster that yields bit names that do not contain `@`.

--- examples/cinderella/cinderella_stepmother.py
+++ examples/cinderella/cinderella_stepmother.py
@@ -1,38 +1,23 @@
 #!/usr/bin/env python
-"""Creates some benchmarks for the discretized
-cinderella-stepmother game.
-"""
-import subprocess
-
-
+"""Dump discretized cinderella-stepmother games."""
 TEMPLATE = """\
-# {comment}
+/* {comment} */
 
-[INPUT]
 {inLines}
-
-[OUTPUT]
 {outLines}
-e:0...{nofBucketsMinusOne}
+sys int(0, {nofBucketsMinusOne}) e;
 
-[ENV_INIT]
+assume ltl {{
 {envInit}
+&& []({stepMotherRestrictionLine})
+&& []<>(true)
+}}
 
-[ENV_TRANS]
-{stepMotherRestrictionLine}
-
-[ENV_LIVENESS]
-1
-
-[SYS_INIT]
+assert ltl {{
 {sysInit}
-
-[SYS_TRANS]
-{sysTransA}
-{sysTransB}
-
-[SYS_LIVENESS]
-1
+&& []({sysTransA})
+&& []<>(true)
+}}
 
 """
 # ==================================
@@ -63,7 +48,7 @@ translator = ('../../../slugs/tools/'
               'StructuredSlugsParser/compiler.py')
 
 
-def make_code(params, slugsin_file):
+def make_code(params, pml_fname):
     (nofBuckets, cinderellaPower,
      bucketCapacity, stepmotherPower) = params
     comment = "n{a} c{b}, {c} by {d}".format(
@@ -72,19 +57,19 @@ def make_code(params, slugsin_file):
         c=bucketCapacity,
         d=stepmotherPower)
     inlines = [
-        "x" + str(i) + ":0..." + str(stepmotherPower)
+        'env int(0, {b}) x{i};'.format(i=i, b=stepmotherPower)
         for i in xrange(0, nofBuckets)]
     outlines = [
-        "y" + str(i) + ":0..." + str(bucketCapacity)
+        'sys int(0, {b}) y{i};'.format(i=i, b=bucketCapacity)
         for i in xrange(0, nofBuckets)]
     envinit = [
-        "x" + str(i) + " = 0"
+        'x{i} = 0'.format(i=i)
         for i in xrange(0, nofBuckets)]
     sysinit = [
-        "y" + str(i) + " = 0"
+        'y{i} = 0'.format(i=i)
         for i in xrange(0, nofBuckets)]
     sys_action = [
-        "!((e<=" + str(i) + " & e+" +
+        "!((e<=" + str(i) + " && e+" +
         str(cinderellaPower) + " > " + str(i) +
         ") | (e+" + str(cinderellaPower - 1) +
         " >= " + str(i+nofBuckets) + ")) -> y" + str(i) +
@@ -92,7 +77,7 @@ def make_code(params, slugsin_file):
         "'"
         for i in xrange(0, nofBuckets)]
     env_action = [
-        "((e<=" + str(i) + " & e+" +
+        "((e<=" + str(i) + " && e+" +
         str(cinderellaPower) + " > " +
         str(i) + ") | (e+" + str(cinderellaPower - 1) +
         " >= " + str(i + nofBuckets) +
@@ -100,28 +85,28 @@ def make_code(params, slugsin_file):
         "'"
         for i in xrange(0, nofBuckets)]
     restrict = [
-        "x" + str(i) + "'"
+        "x{i}'".format(i=i)
         for i in xrange(0, nofBuckets)]
     text = TEMPLATE.format(
         comment=comment,
-        inLines="\n".join(inlines),
-        outLines="\n".join(outlines),
+        inLines='\n'.join(inlines),
+        outLines='\n'.join(outlines),
         nofBucketsMinusOne=nofBuckets - 1,
         stepMotherRestrictionLine=(
             "+".join(restrict) + " <= " + str(stepmotherPower)),
-        envInit="\n".join(envinit),
-        sysInit="\n".join(sysinit),
-        sysTransA="\n".join(sys_action),
-        sysTransB="\n".join(env_action))
-    with open(fname, 'w') as f:
+        envInit=conj(envinit),
+        sysInit=conj(sysinit),
+        sysTransA=conj(sys_action),
+        sysTransB=conj(env_action))
+    with open(pml_fname, 'w') as f:
         f.write(text)
-    # to slugsin
-    s = subprocess.check_output([translator, fname])
-    with open(slugsin_file, 'w') as f:
-        f.write(s)
+
+
+def conj(x):
+    return '\n && '.join('(' + u + ')' for u in x)
 
 
 if __name__ == '__main__':
     for i, p in enumerate(configurations):
-        slugsin_file = 'slugsin/cinderella_{i}.txt'.format(i=i)
-        make_code(p, slugsin_file)
+        pml_fname = 'pml/cinderella_{i}.txt'.format(i=i)
+        make_code(p, pml_fname)
