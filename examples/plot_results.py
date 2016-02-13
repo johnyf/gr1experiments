7cda8f20d631bbe6ae28bcdfd9cd49930adb9494
Ioannis Filippidis <jfilippidis@gmail.com>
2016-02-12 23:41:26 -0800

    MAI: change plot range

--- examples/plot_results.py
+++ examples/plot_results.py
@@ -29,8 +29,8 @@ def plot_report():
         #'bunny/runs_slugs/': (2, 63),
         #'bunny_goals/runs/': (2, 32),
         #'bunny_goals/runs_slugs/': (2, 33),
-        'synt15/runs_slugs/': (2, 33),
-        #'synt15/runs/': (2, 79),
+        #'synt15/runs_slugs/': (2, 33),
+        'synt15/runs/': (2, 65),
         #'bunny/runs/': (2, 97)
     }
     for path, (first, last) in paths.iteritems():
