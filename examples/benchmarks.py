230257502be5accd27143b3b5c236520d76b4532
Ioannis Filippidis <jfilippidis@gmail.com>
2016-02-12 22:07:11 -0800

    MAI: reduce `gr1x` logging verbosity from debug to info

--- examples/benchmarks.py
+++ examples/benchmarks.py
@@ -110,7 +110,7 @@ def run_gr1x(slugsin_file, strategy_file,
     proc = psutil.Process()
     proc.cpu_affinity(affinity)
     # log verbosity
-    level = logging.DEBUG
+    level = logging.INFO
     log = logging.getLogger(GR1X_LOG)
     log.setLevel(level)
     # dump log
