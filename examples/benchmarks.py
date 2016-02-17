fd801ab67b84a4291ddf1de84f4a09eff078100d
Ioannis Filippidis <jfilippidis@gmail.com>
2016-02-17 15:28:01 -0800

    MAI: show last one, instead of last + 1

--- examples/benchmarks.py
+++ examples/benchmarks.py
@@ -52,7 +52,7 @@ def run_parallel():
     n = first
     for j in xrange(repetitions):
         m = n + concurrent
-        print('spawn {n} to {m}'.format(n=n, m=m))
+        print('spawn {n} to {last}'.format(n=n, last=m - 1))
         jobs = list()
         for i in xrange(n, m):
             d = dict(
