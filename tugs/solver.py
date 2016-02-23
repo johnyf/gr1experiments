b6775a294a6700c0d8b4cf0e0d31940c6bfdc985
Ioannis Filippidis <jfilippidis@gmail.com>
2016-02-23 01:31:14 -0800

    EXP: variant with feedback and loose z propagation
    this variant is closer to Jtlv, Anzu, Marduk.

--- tugs/solver.py
+++ tugs/solver.py
@@ -25,8 +25,8 @@ BINARY_CONJ = False  # overrides `DEFER_Z`
 DEFER_Z = False
 TWO_MANAGERS = True
 MEMOIZE_ITERATES = False
-FEEDBACK = False
-TIGHT = True
+FEEDBACK = True
+TIGHT = False
 # constants
 REORDERING_LOG = 'reorder'
 COUNTER = '_jx_b'
